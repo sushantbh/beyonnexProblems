5db6af435e4d189aab5f07d4380d323bfd92f15a
sushantbh <40732876+sushantbh@users.noreply.github.com>
2023-08-30 03:58:02 +0530

    Added changes for handling cases where special characters and spaces could also occur in the text.

--- src/com/beyonnex/problems/AnagramProblem.java
+++ src/com/beyonnex/problems/AnagramProblem.java
@@ -7,16 +7,17 @@ public class AnagramProblem {
         {
             return false;
         }
+// For accomodating special characters and spaces, have increased the size of array, size of array could be taken as 26 as well, if we are ensured that there will be only alphabetic character with no case check
+        int a[] = new int[95];
 
-        int a[] = new int[26];
-
+// toLowerCase() can be removed if we want to treat uppercase and lowercase as a seperate letter.
         for(char c:s.toLowerCase().toCharArray())
         {
-            a[c-97]++;
+            a[c-32]++;   // If we would only be considering alphabetic characters with no case check, we can change 32 to 97
         }
         for(char c:t.toLowerCase().toCharArray())
         {
-            a[c-97]--;
+            a[c-32]--;
         }
 
         for(int i:a)
@@ -32,10 +33,10 @@ public class AnagramProblem {
     public static void main(String[] args) {
         //Some Test Cases
         System.out.println(isAnagram("Test","stet"));
-        System.out.println(isAnagram("aaaaaabbbbaaaccaaccabb","abc"));
+        System.out.println(isAnagram("aaaaaabbbbaaaccaaccabb","abc")); 
         System.out.println(isAnagram("","abcdefg"));
         System.out.println(isAnagram("abcdef",""));
         System.out.println(isAnagram("abcdef",null));
-        System.out.println(isAnagram("abcdefghijklmnopqrstuvwxyz","qwertyuiopasdfghjklzxcvbnm"));
+        System.out.println(isAnagram("abcdefghijklmn opqrstuvwxy%^z","qwertyuiop^%asdfgh jklzxcvbnm")); // Test case with special characters and spaces
     }
 }
