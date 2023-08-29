8d8ed03f26523b15256618490089d103eb6b96ee
sushantbh <40732876+sushantbh@users.noreply.github.com>
2023-08-30 01:41:12 +0530

    Added code for anagram problem

--- src/com/beyonnex/problems/AnagramProblem.java
+++ src/com/beyonnex/problems/AnagramProblem.java
@@ -1,1 +1,41 @@
+package com.beyonnex.problems;
 
+public class AnagramProblem {
+    public static boolean isAnagram(String s, String t) {
+
+        if(s==null || t==null ||  s.length()!=t.length())
+        {
+            return false;
+        }
+
+        int a[] = new int[26];
+
+        for(char c:s.toLowerCase().toCharArray())
+        {
+            a[c-97]++;
+        }
+        for(char c:t.toLowerCase().toCharArray())
+        {
+            a[c-97]--;
+        }
+
+        for(int i:a)
+        {
+            if(i!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        //Some Test Cases
+        System.out.println(isAnagram("Test","stet"));
+        System.out.println(isAnagram("aaaaaabbbbaaaccaaccabb","abc"));
+        System.out.println(isAnagram("","abcdefg"));
+        System.out.println(isAnagram("abcdef",""));
+        System.out.println(isAnagram("abcdef",null));
+        System.out.println(isAnagram("abcdefghijklmnopqrstuvwxyz","qwertyuiopasdfghjklzxcvbnm"));
+    }
+}
